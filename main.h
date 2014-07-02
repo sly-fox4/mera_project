e0be725a36876a1d4fb61c668ff9069ad3c321f2
Serge <sly_fox4@mail.ru>
2014-07-02 23:15:35 +0400

    transfer of "15"

--- main.h
+++ main.h
@@ -7,10 +7,14 @@
 #include <Vcl.Controls.hpp>
 #include <Vcl.StdCtrls.hpp>
 #include <Vcl.Forms.hpp>
+#include <Vcl.ComCtrls.hpp>
 //---------------------------------------------------------------------------
 class TForm1 : public TForm
 {
 __published:	// IDE-managed Components
+	TDateTimePicker *DateTimePicker1;
+	TButton *Button1;
+	void __fastcall Button1Click(TObject *Sender);
 private:	// User declarations
 public:		// User declarations
 	__fastcall TForm1(TComponent* Owner);
